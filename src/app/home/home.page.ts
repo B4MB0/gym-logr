37f408bbe2f7de5f9614ad5a2922da1420abad2d
AdrienWeber <adrienweber60@gmail.com>
2023-05-04 20:23:37 +0200

    first commit with the first app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { RefresherCustomEvent } from '@ionic/angular';
+import { AlertController, RefresherCustomEvent, ToastController } from '@ionic/angular';
 import { MessageComponent } from '../message/message.component';
 
 import { DataService, Message } from '../services/data.service';
@@ -10,16 +10,61 @@ import { DataService, Message } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  private data = inject(DataService);
-  constructor() {}
+  // hier kommt eigener Code rein!
+  // am besten mit etwas kleinem anfangen damit es einfach bleibt nichts kompliziertes!
+  // variable exerciseList mit einem Array gefüllt mit Elementen
+  exerciseList = JSON.parse(localStorage.getItem('Exercises')) || [];
+  // AlertController definieren;
+  constructor(private alertController: AlertController, private toastController: ToastController) {}
 
-  refresh(ev: any) {
-    setTimeout(() => {
-      (ev as RefresherCustomEvent).detail.complete();
-    }, 3000);
+  // eigene Funktion schrreiben;
+
+  async addExercise(){
+    // 49:11 im video erklärt das
+    const alert = await this.alertController.create({
+      header: 'Neue Übung hinzufügen:',
+      buttons: [{
+        text: 'Speichern',
+        handler: (values) => {
+          this.exerciseList.push(values[0]);
+          this.save();
+        }
+      }],
+      inputs: [
+        {
+          placeholder: 'Neue Übung',
+        },
+        //  {
+        //    type: 'number',
+        //    placeholder: 'Gewicht',
+        //    min: 1,
+        //    max: 10000,
+        //  },
+      ]
+    });
+
+    await alert.present();
   }
 
-  getMessages(): Message[] {
-    return this.data.getMessages();
+  async presentToast(msg){
+  const toast = await this.toastController.create({
+    message: msg,
+    duration: 1000,
+    position: 'top'
+  });
+
+  await toast.present();
   }
+  deleteItem(i){
+    this.exerciseList.splice(i, 1);
+    this.save();
+    this.presentToast('Die Übung wurde gelöscht!');
+  }
+
+  save(){
+    this.presentToast('Die Übung wurde hinzugefügt!')
+    let itemsAsText = JSON.stringify(this.exerciseList);
+    localStorage.setItem('Exercises', itemsAsText);
+  }
+  
 }
